68daab6c59f3b3832c37e2bcb0febe4bd1bbd381
iibrahim15 <iibrahim15@myseneca.ca>
2024-02-12 23:34:47 -0500

    Modified the file

--- lab1.py
+++ lab1.py
@@ -1,9 +1,12 @@
 def calculate_age():
-    birth_year = input("Enter your birth year: ")
-    birth_year = int(birth_year)
-    current_year = 2024 
-    age = current_year - birth_year
-    print("You are " + str(age) + " years old.")
+    try:
+        birth_year = input("Enter your birth year: ")
+        birth_year = int(birth_year)
+        current_year = 2024  # replace with the current year
+        age = current_year - birth_year
+        print("You are " + str(age) + " years old.")
+    except ValueError:
+        print("Please enter an int")
 calculate_age()
 
 def helloWorld():
