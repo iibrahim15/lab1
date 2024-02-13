d10cee3294838a94fabc970c97c8a1049228bba0
Issack Ibrahim <156141967+iibrahim15@users.noreply.github.com>
2024-02-12 22:59:26 -0500

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,9 +1,6 @@
-from datetime import datetime
 def calculate_age():
     birth_year = input("Enter your birth year: ")
     birth_year = int(birth_year)
     current_year = 2024 
     age = current_year - birth_year
     print("You are " + str(age) + " years old.")
-
-calculate_age()
